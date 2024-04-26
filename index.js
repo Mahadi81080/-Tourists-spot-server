e9e369d2017539b2ca24474cbcc7b07b230a0d81
Mahadi810803 <mahedihasan81080@gmail.com>
2024-04-26 10:58:23 +0600

    get spot data

--- index.js
+++ index.js
@@ -25,6 +25,11 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     const spotCollection = client.db("spotDB").collection("spot");
+    app.get('/addSpot',async(req,res)=>{
+      const cursor = spotCollection.find()
+      const result =await cursor.toArray()
+      res.send(result)
+    })
     app.post('/addSpot',async(req,res)=>{
       const newSpot =req.body;
       console.log(newSpot);
